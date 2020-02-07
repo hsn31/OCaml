58b1e965b6c8f5486361ea9125ff4002194f9f44
hsahraou@etud.insa-toulouse.fr <hsahraou@etud.insa-toulouse.fr>
2020-02-07 13:44:55 +0100

    Initialisation et définition des objectifs

---
+++ src/fordfulkerson.ml
@@ -0,0 +1,15 @@
+(*
+* Déroulé de l'Algorithme
+* 
+* Trouver un chemin qui augmente le flot max.
+* Trouver sur le chemin concerné le flot max (compute the bottleneck capacity : calculer la capacité du goulot d'étranglement)
+* Mettre à jour les labels des arcs concernés + le flot max
+* 
+* Opération à répéter tant qu'il y a des chemins disponibles (à partir de la source, et qu'il reste de la capacité)
+* Attention label négatif impossible
+*
+* Une fois l'algo terminé, calculer le flot max comme la somme des labels entrants - sortants de l'ID final
+* 
+* 
+*)
+
---
+++ src/tools.ml
@@ -0,0 +1,13 @@
+open Graph
+
+(* assert false is of type ∀α.α, so the type-checker is happy. *)
+
+(* returns a new graph having the same nodes than gr, but no arc. (code : one line)*)
+let clone_nodes gr = assert false
+
+(* maps all arcs of gr by function f. (⩽3 lines)*)
+let gmap gr f = assert false
+
+(*adds n to the value of the arc between id1 and id2. If the arc does not exist, it is created.*)
+
+let add_arc g id1 id2 n = assert false
